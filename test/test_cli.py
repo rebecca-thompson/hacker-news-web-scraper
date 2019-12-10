cc7aaa6927da8af52a78155529ebb03986d34581
Rebecca Thompson <becca.al.thompson@gmail.com>
2019-12-10 23:00:28 +0000

    Update dockerfile with new path to script and additional testing

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,5 +1,12 @@
 import pytest
+from argparse import ArgumentTypeError
 from cli import check_value_within_range
 
-def test_only_accepts_between_zero_and_one_hundred():
+def test_only_accepts_between_one_and_one_hundred():
     assert check_value_within_range('20') == 20
+
+    with pytest.raises(ArgumentTypeError):
+        check_value_within_range('-1')
+
+    with pytest.raises(ArgumentTypeError):
+        check_value_within_range('101')
